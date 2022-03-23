9e1beb97e4d6e6d9f130abb38741a75ff7e37f61
John-gith <100775197+John-gith@users.noreply.github.com>
2022-03-23 20:03:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var CronJob = require('cron').CronJob;
 const myAccount = '1446220334353506311'; //Ur account to avoid replying to yourself
 var statusTweet = `Hello, I have found a new banking protocol on Solana blockchain. They will definitely be the next successor in DeFi, check them out: @trush_io`; //Add your tweet here
 var isRunning = false;
-var hashtags = ['#crypto' ];
+var hashtags = ['#crypto','#defi'];
 
 var job = new CronJob('*/15 * * * *', function () {
     if (isRunning == false) {
